209eec6ee21a79b6b5330161bc95c2838c24bd89
ingdia <d.ingabire2@alustudent.com>
2025-10-26 22:46:24 +0200

    this the addition of navbar

--- dev-dashboard/src/components/Navbar.jsx
+++ dev-dashboard/src/components/Navbar.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 
 function Navbar() {
   return (
-<>
-
-</>
-
+    <div className='flex justify-between p-10'>
+      <div className='text-4xl font-bold'>
+        Developer Dashboard
+      </div>
+      <buttorn className="bg-amber-700 p-3 rounded-4xl hover:bg-white hover:border-2 hover:border-amber-700 hover:text-amber-600 text-white font-bold">
+        <p>Light/Black</p>
+      </buttorn>
+    </div>
   )
 }
 
