9720af2680aa93287fea88f4ad5a51ecfb44c4d9
ingdia <d.ingabire2@alustudent.com>
2025-10-26 23:19:40 +0200

    added the github feature skeleton

--- dev-dashboard/src/components/GitHubCard.jsx
+++ dev-dashboard/src/components/GitHubCard.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import image from "../assets/image.png"
 
 function GitHubCard() {
   return (
     <div>
-      
+      <h1>Github</h1>
+      <div>
+         <img src={image}/>
+      </div>
+      <div>
+         <div>
+            <h2> 54 </h2>
+            <p> repo</p>
+         </div>
+         <div>
+            <h2> 54 </h2>
+            <p> repo</p>
+         </div>
+         <div>
+            <h2> 54 </h2>
+            <p> repo</p>
+         </div>
+      </div>
     </div>
   )
 }
--- dev-dashboard/src/dashboard/Home.jsx
+++ dev-dashboard/src/dashboard/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import WeatherCard from '../components/WeatherCard'
+import GitHubCard from '../components/GitHubCard'
 
 function Home() {
   return (
     <div className='bg-amber-200 min-h-screen p-20'>
       <WeatherCard/>
+      <GitHubCard/>
     </div>
   )
 }
